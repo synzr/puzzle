8a5cc4bf7012bd7f49a78fc17b3077c60c82f32f
mikhail <sora@synzr.ru>
2023-04-04 21:40:59 +0500

    fix: token added to push register result

--- routes/neptune/push.js
+++ routes/neptune/push.js
@@ -4,20 +4,34 @@
  */
 
 const { Router } = require('express')
+const jwt = require('jsonwebtoken')
+
 const byPfSessionTokenMiddleware = require('../../middlewares/by-pf-session-token')
 
 const router = new Router()
 
 router.post('/token/register/forClient', byPfSessionTokenMiddleware, (req, res) => {
   const logger = req.app.get('logger')
+  const jwtSecret = req.app.get('jwtSecret')
 
+  const pushToken = jwt.sign({
+    tokenType: 'PUSH_NOTIFICATION_TOKEN',
+    userId: res.locals.user.id,
+    gameId: res.locals.game.id
+  }, jwtSecret, {
+    expiresIn: '24h'
+  })
+  
   logger.log({
     level: 'info',
     message: `User ${res.locals.user.id} registered to the push notifications.`,
     details: { userId: res.locals.user.id }
   })
 
-  return res.json({ success: true })
+  return res.json({
+    success: true,
+    data: { pushToken } 
+  })
 })
 
 module.exports = router
